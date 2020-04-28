38fb46bd54cfa20fb1f7c17a76c151b163977329
winkers <smmehdisharifi@gmail.com>
2020-04-29 00:43:55 +0430

    Test

--- calculateReward.m
+++ calculateReward.m
@@ -6,9 +6,15 @@ function reward=calculateReward(Receiver)
     else
         Trust=(sapv(Receiver.id)/((sapv(Receiver.id)+rapv(Receiver.id))))*10;
     end
-    disp(Trust);
-    E=Receiver.E
-    T=Receiver.T
-    reward=E-T+Trust
-
+    E=Receiver.E;
+    T=Receiver.T;
+    reward=E-T+Trust;
+    
+    disp(['candidate #',num2str(Receiver.id)]);
+    disp(['sapv ',num2str(sapv(Receiver.id))]);
+    disp(['rapv ',num2str(rapv(Receiver.id))]);
+    disp(['Trust ',num2str(Trust)]);
+    disp(['E ',num2str(E)]);
+    disp(['T ',num2str(T)]);
+    disp('---------');
 end
