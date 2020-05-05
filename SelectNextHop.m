2d90dd90a3c3d3ddba86dc5fa7678b02328900a1
shamim <shamimeh.ahmadinezhad@gmail.com>
2020-05-06 00:36:19 +0430

    implement calculatenextQ function
    implement calculatenextQ function

--- SelectNextHop.m
+++ SelectNextHop.m
@@ -7,7 +7,8 @@ function [ nextHop] = SelectNextHop( Sender, Model, Neighbors, Sensors, dissink)
     for j=1:n
         if (Neighbors(Sender,j)==1 && Sensors(j).E>0)
             reward=calculateReward(Sensors(j),dissink);
-            Q(Sender,j)=Q(Sender,j) + Model.alpha*reward;
+            nextQ=cauculatenextQ(Sensors(j),Model,Neighbors,Q);
+            Q(Sender,j)=Q(Sender,j) + Model.alpha*(reward+(Model.gamma*nextQ)-Q(Sender,j));
            % Q(Sender,j)=Q(Sender,j) + Model.Lr*(reward(Sender,j));
             %candidates=[candidates, j];
         end
---
+++ cauculatenextQ.m
@@ -0,0 +1,19 @@
+function nextQ=cauculatenextQ(Receiver,Model,Neighbors,Q)
+
+    n=Model.n;
+    nQs=[];
+    
+    for b=1:n
+        if (Neighbors(Receiver.id,b)==1)
+             nQs = [nQs, Q(Receiver.id,b)];
+        end
+    end
+    
+    if(isempty(nQs))
+        nextQ = -1;
+    else
+        nQs= sort(nQs,'descend');
+        nextQ=nQs(1);
+    end
+
+end
